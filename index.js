d4694704215c126e47c91157be2eb6aa03c39dc8
AllWKA <bryanjaramillobaldeon@gmail.com>
2021-06-05 10:34:17 +0100

    refactor code:
    - extract switch for methods
    - create Object of command handler
    - refactor test command handler

--- index.js
+++ index.js
@@ -1,124 +1,97 @@
-const fs = require('fs-extra');
 require('dotenv/config');
 const configTel = require("./configTel.json");
 const TelegramBot = require('node-telegram-bot-api');
-let list = [];
+
+let participants = [];
 
 const botTel = new TelegramBot(process.env.TOKEN_TELEGRAM, {
     polling: true
 });
 
+function start(message) {
+    botTel.sendMessage(message.chat.id, "¡Hola! He llegado a Telegram")
+}
+
+function add(message) {
+    participants.push({person: message.from.first_name});
+    let count = 0;
+    for (let participant of participants) {
+        count++;
+        if (message.from.first_name === participant.person) {
+            botTel.sendMessage(message.chat.id, message.from.first_name + " eres el número: " + count);
+            break;
+        }
+    }
+    botTel.sendMessage(message.chat.id, "Añadido: " + message.from.first_name);
+}
+
+async function rafflebut(message, args) {
+    let tu = (args[0]) - 1
+    let exclude = (args[1]) - 1;
+    let random = Math.floor(Math.random() * (participants.length));
+    let checkList = false;
+    for (let persona of participants) {
+        if (persona.person !== "none" && persona.person !== participants[tu].person && persona.person !== participants[exclude].person) {
+            checkList = true;
+            break;
+        }
+    }
+    if (checkList) {
+        while (exclude === random || random === tu || participants[random].person === "none") {
+            random = Math.floor(Math.random() * (participants.length));
+        }
+        await botTel.sendMessage(message.from.id, "Te ha tocado: " + participants[random].person)
+        participants[random] = {person: "none"};
+    } else {
+        await botTel.sendMessage(message.from.id, "No hay nadie mas en la lista");
+    }
+}
+
+async function raffle(message, args) {
+    let tua = (args[0]) - 1
+    let randoma = Math.floor(Math.random() * (participants.length));
+    let checkLista = false;
+    for (let personaA of participants) {
+        if (personaA.person !== "none" && personaA.person !== participants[tua].person) {
+            checkLista = true;
+            break;
+        }
+    }
+    if (checkLista) {
+        while (randoma === tua || participants[randoma].person === "none") {
+            randoma = Math.floor(Math.random() * (participants.length));
+        }
+        await botTel.sendMessage(message.from.id, "Te ha tocado: " + participants[randoma].person)
+        participants[randoma] = {
+            person: "none"
+        };
+
+    } else {
+        await botTel.sendMessage(message.from.id, "No hay nadie mas en la lista");
+    }
+}
+
+function check(message) {
+    botTel.sendMessage(message.chat.id, "La lista es la siguiente: ").then(() => {
+        for (let persona of participants) {
+            botTel.sendMessage(message.chat.id, persona.person);
+        }
+    });
+}
+
+function test(message) {
+    ["Amanda", "Bob", "Pepe", "Tomate", "Juan Habichuelo"].forEach(participant => participants.push({person: participant}))
+    participants.forEach((participant, i) => botTel.sendMessage(message.chat.id, participant.person + " tiene el numero " + i))
+}
+
+const commandHandlers = {start, check, test, add, rafflebut, raffle}
+
 botTel.on("message", async message => {
-    if (message.text != undefined) {
+    if (message.text !== undefined) {
         if (message.text.indexOf(configTel.prefix) !== 0) return;
 
         const args = message.text.slice(configTel.prefix.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
-        switch (command) {
-
-            case "start":
-
-                botTel.sendMessage(message.chat.id, "¡Hola! He llegado a Telegram");
-                break;
-
-            case "add":
-                botTel.sendMessage(message.chat.id, "Añadido: " + message.from.first_name);
-                list.push({
-                    person: message.from.first_name
-                });
-                let count = 0;
-                for (let persona of list) {
-                    count++;
-                    if (message.from.first_name === persona.person) {
-                        botTel.sendMessage(message.chat.id, message.from.first_name + " eres el número: " + count);
-                        break;
-                    }
-                }
-
-                break;
-
-            case "rafflebut":
-                let tu = (args[0]) - 1
-                let exclude = (args[1]) - 1;
-                let random = Math.floor(Math.random() * (list.length - 0) + 0);
-                let checkList = false;
-                for (let persona of list) {
-                    if (persona.person !== "none" && persona.person !== list[tu].person && persona.person !== list[exclude].person) {
-                        console.log("entro")
-                        checkList = true;
-                        break;
-                    }
-                }
-                if (checkList) {
-                    while (exclude === random || random === tu || list[random].person === "none") {
-                        random = Math.floor(Math.random() * (list.length - 0) + 0);
-                    }
-                    await botTel.sendMessage(message.from.id, "Te ha tocado: " + list[random].person)
-                    list[random] = {
-                        person: "none"
-                    };
-                    break;
-
-                } else {
-                    await botTel.sendMessage(message.from.id, "No hay nadie mas en la lista");
-                    break;
-                }
-
-                case "raffle":
-                    let tua = (args[0]) - 1
-                    let randoma = Math.floor(Math.random() * (list.length - 0) + 0);
-                    let checkLista = false;
-                    for (let personaA of list) {
-                        if (personaA.person !== "none" && personaA.person !== list[tua].person) {
-                            checkLista = true;
-                            break;
-                        }
-                    }
-                    if (checkLista) {
-                        while (randoma === tua || list[randoma].person === "none") {
-                            randoma = Math.floor(Math.random() * (list.length - 0) + 0);
-                        }
-                        await botTel.sendMessage(message.from.id, "Te ha tocado: " + list[randoma].person)
-                        list[randoma] = {
-                            person: "none"
-                        };
-                        break;
-
-                    } else {
-                        await botTel.sendMessage(message.from.id, "No hay nadie mas en la lista");
-                        break;
-                    }
-
-                    case "check":
-                        botTel.sendMessage(message.chat.id, "La lista es la siguiente: ").then(done => {
-                            for (let persona of list) {
-                                botTel.sendMessage(message.chat.id, persona.person);
-                            }
-                        });
-                        break;
-
-                    case "test":
-                        console.log("El puto de: " + message.from.first_name + " esta probandome")
-                        list.push({
-                            person: "Amanda"
-                        });
-                        list.push({
-                            person: "Bob"
-                        });
-                        list.push({
-                            person: "Pepe"
-                        });
-                        list.push({
-                            person: "Tomate"
-                        });
-                        list.push({
-                            person: "Juan Habichuelo"
-                        });
-                        let counts = 0
-                        for (let persona of list) {
-                            counts++
-                            botTel.sendMessage(message.chat.id, persona.person + " tiene el numero " + counts);
-                        }
-        }
+        commandHandlers[command](message, args)
     }
 })
